11e12bb5a1170e27447b3561bad7e279c504b166
vlad <gosvlad236@gmail.com>
2024-01-21 22:05:44 +0200

    added changes to task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,10 +1,10 @@
-const inputElement = document.querySelector('input')
-const btnCreate = document.querySelector('[data-create]')
-const btnDestroy = document.querySelector('[data-destroy]')
-const boxDiv = document.querySelector('#boxes')
+const inputField = document.querySelector('input')
+const createButton = document.querySelector('[data-create]')
+const destroyButton = document.querySelector('[data-destroy]')
+const divContainer = document.querySelector('#boxes')
 
-btnCreate.addEventListener('click', createDivHandler)
-btnDestroy.addEventListener('click', destroyBoxes)
+createButton.addEventListener('click', createBoxes)
+destroyButton.addEventListener('click', destroyBoxes)
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -12,35 +12,31 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-function createDivHandler(event) {
-  const amountBoxes = Number(inputElement.value)
-  
-  if (amountBoxes >= 1 && amountBoxes <= 100) {
-    inputElement.value = ''
-    createBoxes(amountBoxes);
-  }
-}
-
 function createBoxes(amount) {
   destroyBoxes()
-  let startSize = 30;
+  const totalBoxes = Number(inputField.value)
+  
+  if (totalBoxes >= 1 && totalBoxes <= 100) {
+    inputField.value = ''
+    createBoxes(totalBoxes);
+  }
+  let startedSize = 30;
   const step = 10;
-  const divArray = [];
+  const divContainerArray = [];
 
   for (let i = 0; i < amount; i++) {
-    const newDiv = document.createElement('div')
-    newDiv.style.width = `${startSize}px`
-    newDiv.style.height = `${startSize}px`
-    newDiv.style.backgroundColor = getRandomHexColor()
-    
-    divArray.push(newDiv)
-    startSize += step
+    const createdDiv = document.createElement('div')
+    createdDiv.style.backgroundColor = getRandomHexColor()
+    createdDiv.style.width = `${startedSize}px`
+    createdDiv.style.height = `${startedSize}px`
+    divContainerArray.push(createdDiv)
+    startedSize += step
   }
-  boxDiv.append(...divArray)
-  console.log('createBoxes event!', amount);
+  divContainer.append(...divContainerArray)
+  // console.log('createBoxes event!', amount);
 }
 
 function destroyBoxes(event) {
-  boxDiv.innerHTML = ''
-  console.log('destroyBoxes event!');
+  divContainer.innerHTML = ''
+  // console.log('destroyBoxes event!');
 }
